6ca524519bd108ac89b023b491e80256927bcd27
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-03-09 06:30:18 +0200

    fixed Type error: Type 'PostgrestBuilder<{ title: any; created_at: any; content: any; },

--- app/embed/[id]/Pastes.tsx
+++ app/embed/[id]/Pastes.tsx
@@ -22,11 +22,11 @@ export  function Pastes({
  
   return (
     <div key={pasteInfo.data.id} className='bg-white p-4 rounded-lg shadow-md'>
-      <p className='text-xl font-bold'>{pasteInfo.data.title}</p>
+      {/* <p className='text-xl font-bold'>{pasteInfo.data.title}</p>
 
       <p className='text-neutral-600 text-xs mb-4'>Created at: {new Date(pasteInfo.data.created_at).toLocaleString()}</p>
-          <Markdown content={pasteInfo.data.content} />
-
+          <Markdown content={pasteInfo.data.content} /> */}
+<iframe src={`https://localhost:3000/embed/${pasteInfo.data.id}`} width="600" height="400" style={{border:"1px solid #ccc"}}></iframe>
   </div>
   )
 }
--- app/embed/[id]/page.tsx
+++ app/embed/[id]/page.tsx
@@ -24,7 +24,7 @@ export default async function Page({ params }:PageProps) {
         <LoadingDots />
       </div>
   }>
-
+{/* @ts-ignore */}
       <Pastes paste={paste} />
     </Suspense>
   )
